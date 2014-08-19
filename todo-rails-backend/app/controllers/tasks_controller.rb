5f315aa5aaa7e42f7666c48ffe1a66169d000ade
Raman Bedi <ramanbedi1989@gmail.com>
2014-08-19 12:34:06 +0530

    add tasks api

--- todo-rails-backend/app/controllers/tasks_controller.rb
+++ todo-rails-backend/app/controllers/tasks_controller.rb
@@ -1,50 +1,77 @@
 class TasksController < ApplicationController
-  # GET /tasks
-  # GET /tasks.json
   def index
-    @tasks = Task.all
-
-    render json: @tasks
-  end
-
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
-    @task = Task.find(params[:id])
-
-    render json: @task
-  end
-
-  # POST /tasks
-  # POST /tasks.json
-  def create
-    @task = Task.new(params[:task])
-
-    if @task.save
-      render json: @task, status: :created, location: @task
+    user = User.find(params[:user_id])
+    if user
+      tasks = user.tasks
+      render json: tasks
     else
-      render json: @task.errors, status: :unprocessable_entity
+      render json: { message: 'User not found' }.to_json, status: :not_found
+    end
+  end
+
+  def show
+    user = User.find(params[:user_id])
+    if user
+      task = user.tasks.find(params[:id])
+      if task
+        render json: task, status: :ok
+      else
+        render json: { message: 'Task not found' }.to_json, status: :not_found
+      end
+    else
+      render json: { message: 'User not found' }.to_json, status: :not_found
+    end
+  end
+
+  def create
+    user_id = params[:user_id]
+    task = User.find(user_id).tasks.new(task_params[:task])
+    if task.save
+      render json: task, status: :created
+    else
+      render json: task.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /tasks/1
   # PATCH/PUT /tasks/1.json
   def update
-    @task = Task.find(params[:id])
-
-    if @task.update(params[:task])
-      head :no_content
+    user = User.find(params[:user_id])
+    if user
+      task = user.tasks.find(params[:id])
+      if task && task.update(task_update_params[:task])
+        render json: task, status: :ok
+      else
+        render json: { message: 'Task not found' }.to_json, status: :not_found
+      end
     else
-      render json: @task.errors, status: :unprocessable_entity
+      render json: { message: 'User not found' }.to_json, status: :not_found
     end
   end
 
   # DELETE /tasks/1
   # DELETE /tasks/1.json
   def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
+    user = User.find(params[:user_id])
+    if user
+      task = user.tasks.find(params[:id])
+      if task && task.delete
+        render json: task, status: :ok
+      else
+        render json: { message: 'Task not found' }.to_json, status: :not_found
+      end
+    else
+      render json: { message: 'User not found' }.to_json, status: :not_found
+    end
+  end
 
-    head :no_content
+  private
+
+  def task_params
+    params.permit(:user_id, task: [:title, :description, :complete_till])
+  end
+
+  def task_update_params
+    params.permit(:user_id, :id, task: [:title, :description, :complete_till])
   end
 end
--- todo-rails-backend/config/routes.rb
+++ todo-rails-backend/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :authentication, only: [:create]
   match '/authentication/:email_id/:auth_id', to: 'authentication#destroy', via: :delete, constraints: { email_id: /[^\/]+/ }
 
+  scope 'users/:user_id' do 
+    resources :tasks
+  end
 end
