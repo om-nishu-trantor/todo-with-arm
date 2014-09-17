beca434eef5120b3d54889a695b28e2cf8f5d113
Raman Bedi <ramanbedi1989@gmail.com>
2014-09-17 15:04:10 +0530

    add shake effect

--- todo-yeoman-front-end/app/scripts/controllers/login_controller.js
+++ todo-yeoman-front-end/app/scripts/controllers/login_controller.js
@@ -7,21 +7,29 @@
  * # LoginCtrl
  * Controller of the todosApp
  */
-app.controller('LoginCtrl', ['$scope', '$http', '$location', 'loginService', 'sessionService',function ($scope, $http, $location, loginService, sessionService) {
+ app.controller('LoginCtrl', ['$scope', '$http', '$location', 'loginService', 'sessionService', '$animate', '$timeout',function ($scope, $http, $location, loginService, sessionService, $animate, $timeout) {
   var email, password;
   $scope.loginData = {email: '', password: ''};
   $scope.login = function () {
-    var request = loginService.login($scope.loginData);
-    request.then(
+    if($scope.loginForm.$valid){
+      var request = loginService.login($scope.loginData);
+      request.then(
       function (promise) { // Success callback
         sessionService.setSession(promise.data);
         if(sessionService.isLogin()) $location.path('/dashboard');
       },
       function (error) { // Error callback
-        console.log(error);
         sessionService.isLogin();
-//          TODO add shake effect on fail....
-      }
-    )
+      });
+    }
+    else{
+      var formElement = angular.element('form');
+      $animate.addClass(formElement, 'shake', function() {
+        $timeout(function(){
+          $animate.removeClass(formElement, 'shake');
+        }, 400);
+      });
+    }
+  
   };
 }]);
